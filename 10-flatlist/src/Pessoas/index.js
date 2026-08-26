b577da01a6a76d8bba1f698ea211fa177c34be85
GRAZIELE ALVES SOARES <graziele.asoares@senacsp.edu.br>
2026-08-26 10:38:42 -0300

    flatlista-modal

---
+++ 10-flatlist/src/Pessoas/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+ 
+const styles = StyleSheet.create({
+     areaPessoa:{
+    backgroundColor: "#6245f5",
+    height: 250,
+    marginBottom: 20
+
+   },
+   textoPessoa:{
+    color: "#fff",
+    fontSize: 25,
+   }
+
+})
+
+class Pessoa extends Component {
+   render(){
+      return(
+         <View style={styles.areaPessoa}>
+            <Text style={styles.textoPessoa}>Id: {this.props.data.id}</Text>
+            <Text style={styles.textoPessoa}>Nome: {this.props.data.nome}</Text>
+            <Text style={styles.textoPessoa}>Idade: {this.props.data.idade}</Text>
+            <Text style={styles.textoPessoa}>Email: {this.props.data.email}</Text>
+         </View>
+      )
+   }
+}
+
+export default Pessoa;
