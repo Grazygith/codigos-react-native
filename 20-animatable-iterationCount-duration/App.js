7c6db38e727fe02a254eadec94809778120bba08
GRAZIELE ALVES SOARES <graziele.asoares@senacsp.edu.br>
2026-09-18 09:59:30 -0300

    atualização

---
+++ 20-animatable-iterationCount-duration/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import * as Animatable from 'react-native-animatable';
+
+export default function app(){
+return(
+<View style={styles.container}>
+<Animatable.Text
+style={styles.title}
+//animation= "bounce"
+//iterationCount={3}
+//iterationCount={Infinity}
+animation="tada"
+duration={5000}
+
+>
+Meu aplicativo
+</Animatable.Text>
+</View>
+
+)
+}
+
+const styles = StyleSheet .create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center'
+  },
+  title: {
+    fontSize: 25
+  }
+})
